eb498abe4143c070941f4ac6dc993cce33ced314
Nazrul islam <nazrulislamsuton06@gmail.com>
2023-11-22 20:45:53 +0600

    some-updated

--- social_media/settings.py
+++ social_media/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,12 +117,11 @@ WSGI_APPLICATION = 'social_media.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default="postgres://nazrul:GXc9xm7e5ZvzTRd2tDtBjTMY7ta3Cycc@dpg-clf0jrnjc5ks7393bqng-a.oregon-postgres.render.com/social_media_fs7u",
+        conn_max_age=600,
+    )
 }
 
 
